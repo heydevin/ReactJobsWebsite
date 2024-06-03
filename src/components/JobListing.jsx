409a533be4270a63ad0eb8764a257979ba7dc707
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-06-03 05:56:46 -0700

    Adding dynamic text buttons using State

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if(!showFullDescription) {
+    description = description.substring(0, 170) + '...';
+  }
+
   return (
     <div class="bg-white rounded-xl shadow-md relative">
       <div class="p-4">
@@ -9,9 +17,12 @@ const JobListing = ({ job }) => {
           <h3 class="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div class="mb-5">
-        {job.description}
-        </div>
+        <div class="mb-5">{description}</div>
+
+        <button onClick={() => setShowFullDescription((prevState) => 
+          !prevState)} className="text-indigo-500 mb-5 hover:text-indogo-600">
+          { showFullDescription ? 'Less' : 'More'}
+        </button>
 
         <h3 class="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
