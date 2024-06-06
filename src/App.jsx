1db50110ff471dcf93605093df989a4380d06d4d
Devinlin9 <devinlin@students.cs.ubc.ca>
2024-06-06 00:07:17 -0700

    Delete Job function added with API Request

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
+  // Add Job
   const addJob = async (newJob) => {
     const res = await fetch('/api/jobs', {
       method: 'POST',
@@ -20,17 +21,26 @@ const App = () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(newJob),
-    })
-    return
-  }
+    });
+    return;
+  };
   
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  };
+
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
-        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
